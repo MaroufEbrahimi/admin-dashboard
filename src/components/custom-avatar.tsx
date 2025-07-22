a66059737150d70af65a66cad023fa9a587d3205
marouf ebrahimi <maroufibrahimi7264@gmail.com>
2025-07-22 23:44:13 +0430

    graphql.config file added

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,24 @@
+import { Avatar as AntdAvatar, AvatarProps } from "antd";
+
+type Props = AvatarProps & {
+  name: string;
+};
+
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
+  return (
+    <AntdAvatar
+      alt={"Marouf Ebrahimi"}
+      size={"small"}
+      style={{
+        backgroundColor: "#87d068",
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+      }}
+    >
+      MJ
+    </AntdAvatar>
+  );
+};
+
+export default CustomAvatar;
