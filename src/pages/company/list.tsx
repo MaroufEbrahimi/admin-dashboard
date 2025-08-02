db06842e7c7dfd15b22699b50281912a1a1ffd59
marouf ebrahimi <maroufibrahimi7264@gmail.com>
2025-08-02 21:31:30 +0430

    tables of companies added

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -1,7 +1,19 @@
+import CustomAvatar from "@/components/custom-avatar";
+import { Text } from "@/components/text";
 import { COMPANIES_LIST_QUERY } from "@/graphql/queries";
-import { CreateButton, List, useTable } from "@refinedev/antd";
-import { useGo } from "@refinedev/core";
-import { Table } from "antd";
+import { Company } from "@/graphql/schema.types";
+import { currencyNumber } from "@/utilities";
+import { SearchOutlined } from "@ant-design/icons";
+import {
+  CreateButton,
+  DeleteButton,
+  EditButton,
+  FilterDropdown,
+  List,
+  useTable,
+} from "@refinedev/antd";
+import { getDefaultFilter, useGo } from "@refinedev/core";
+import { Input, Space, Table } from "antd";
 
 export const CompanyList = () => {
   const go = useGo();
@@ -9,6 +21,23 @@ export const CompanyList = () => {
     resource: "companies",
     pagination: {
       pageSize: 12,
+    },
+    sorters: {
+      initial: [
+        {
+          field: "createdAt",
+          order: "desc",
+        },
+      ],
+    },
+    filters: {
+      initial: [
+        {
+          field: "name",
+          operator: "contains",
+          value: undefined,
+        },
+      ],
     },
     meta: {
       gqlQuery: COMPANIES_LIST_QUERY,
@@ -35,7 +64,50 @@ export const CompanyList = () => {
         />
       )}
     >
-      <Table {...tableProps} pagination={{ ...tableProps.pagination }}></Table>
+      <Table {...tableProps} pagination={{ ...tableProps.pagination }}>
+        <Table.Column<Company>
+          dataIndex="name"
+          title="Company Title"
+          defaultFilteredValue={getDefaultFilter("id", filters)}
+          filterIcon={<SearchOutlined />}
+          filterDropdown={(props) => (
+            <FilterDropdown {...props}>
+              <Input placeholder="Search Company" />
+            </FilterDropdown>
+          )}
+          render={(value, record) => (
+            <Space>
+              <CustomAvatar
+                shape="square"
+                name={record.name}
+                src={record.avatarUrl}
+              />
+              <Text style={{ whiteSpace: "nowrap" }}>{record.name}</Text>
+            </Space>
+          )}
+        />
+
+        <Table.Column<Company>
+          dataIndex="totalRevenue"
+          title="Open deals amount"
+          render={(value, company) => (
+            <Text>
+              {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
+            </Text>
+          )}
+        />
+        <Table.Column<Company>
+          dataIndex="id"
+          title="Actions"
+          fixed="right"
+          render={(value) => (
+            <Space>
+              <EditButton hideText size="small" recordItemId={value} />
+              <DeleteButton hideText size="small" recordItemId={value} />
+            </Space>
+          )}
+        />
+      </Table>
     </List>
   );
 };
